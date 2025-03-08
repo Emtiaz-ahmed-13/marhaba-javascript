6467e75b057c66b48394b36b1c4bb11cd6698276
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-08 23:18:56 +0600

    chapter-2 practice 4 done

---
+++ chapter-2/4.js
@@ -0,0 +1,32 @@
+
+//2.20
+const isBambo=true;
+console.log(isBambo);
+
+//2.21
+const isCrushMessageMe=false;
+console.log(isCrushMessageMe);
+
+//2.22
+const celebrating=true;
+console.log(celebrating);
+
+//2.23
+const isHappy=true;
+const hasMoney=false;
+const isHungry=true;
+console.log(isHappy);
+console.log(hasMoney); 
+console.log(isHungry);
+
+//2.24
+const isStudyDone=false;
+console.log(isStudyDone);
+
+//2.25
+const isPhoneCharged=true;
+console.log(isPhoneCharged);
+
+//2.26
+const isDinnerDone=true;
+console.log(isDinnerDone);
