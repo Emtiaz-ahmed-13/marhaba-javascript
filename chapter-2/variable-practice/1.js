533ac5013f3d18c5dd8e3a1cffe2a1292bdc71e8
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-08 22:26:06 +0600

    complete practice 2

---
+++ chapter-2/variable-practice/1.js
@@ -0,0 +1,7 @@
+age=24;
+sibling=2;
+students=40;
+marks=75;
+rooms=3;
+hours=4;
+total=49;
---
+++ chapter-2/variable-practice/2.js
@@ -0,0 +1,18 @@
+
+age=24;
+console.log(age);
+
+booksToRead=1;
+console.log(booksToRead);
+
+wakeUpTime = "7:00";
+console.log(wakeUpTime);
+
+borrow = 0;
+console.log(borrow);
+
+goals=4;
+console.log(goals);
+
+pillow=1;
+console.log(pillow);
