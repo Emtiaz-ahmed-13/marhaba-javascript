377edc6306736c69b481308ba74685a2b7cfd451
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-09 13:29:06 +0600

    complete chapter-2

---
+++ chapter-2/5.js
@@ -0,0 +1,72 @@
+//2.27
+const earn=100;
+const khoroj=40;
+console.log(earn-khoroj);
+
+//2.28
+const totalPencils = 10;
+const pencilsPerUse = 2;
+const timesCanUse = Math.floor(totalPencils / pencilsPerUse);
+
+console.log("If you have " + totalPencils + " pencils and use " + pencilsPerUse + " at a time, you can use them " + timesCanUse + " times.");
+
+//2.29
+const totalMoney = 60;
+const spendEachTime = 15;
+
+const timesCanSpend = Math.floor(totalMoney / spendEachTime);
+const remainingMoney = totalMoney % spendEachTime;
+console.log("If you have " + totalMoney + " taka and spend " + spendEachTime + " taka each time:");
+console.log("You can spend " + timesCanSpend + " times.");
+console.log("After spending, you will have " + remainingMoney + " taka remaining.");
+
+
+//2.30
+const hello="Hello";
+const world="World";
+console.log(hello +  world);
+
+//2.31
+const chal=153;
+const dal=251;
+const total=chal+dal;
+console.log(total);
+
+//2.32
+
+const totalAmount = 500;
+const spendPerTime = 75;
+const timesCanSpendMoney = Math.floor(totalAmount / spendPerTime);
+const remainingAmount = totalAmount % spendPerTime;
+console.log("Tor kache " + totalAmount + " taka ase, protibar tui " + spendPerTime + " taka khoroj korle:");
+console.log("Tui " + timesCanSpendMoney + " bar khoroj korte parbi.");
+console.log("Sheshe tor kache " + remainingAmount + " taka baki thakbe.");
+
+//2.33
+const number = 8;
+const divisor = 3;
+const quotient = Math.floor(number / divisor);
+const remainder = number % divisor;
+console.log(number + " ke " + divisor + " diya vag korle:");
+console.log("Vagfol: " + quotient);
+console.log("Vagsesh: " + remainder);
+
+//2.34
+const number2 = 50;
+const divisor2 = 9;
+const remainder2 = number2 % divisor2;
+console.log(number2 + " ke " + divisor2 + " diya vag dile:");
+console.log("Vagsesh: " + remainder2);
+
+//2.35
+const ban="Bangla";
+const desh="desh";
+console.log(ban + desh);
+
+//2.36
+const number3 = 50;
+const divisor3 = 9;
+const remainder3 = number3 % divisor3;
+
+console.log(number3 + " ke " + divisor3 + " diya vag dile:");
+console.log("Vagsesh: " + remainder3);
