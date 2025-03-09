09e2ae085437bdd7e948b8e05c7eee24a8f8537b
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-10 01:19:01 +0600

    chapter-3 half complete

---
+++ chapter-3/1.js
@@ -0,0 +1,23 @@
+//3.37
+let price=555;
+console.log(typeof price);
+
+//3.38
+let learning=true;
+console.log(typeof learning);
+
+//3.39
+let megaCity="Dhaka";
+console.log(typeof megaCity);
+
+//3.40
+let temperature=102.5;
+console.log(typeof temperature);
+
+//3.41
+let isHungry=true;
+console.log(typeof isHungry);
+
+//3.42
+let isRich=true;
+console.log(typeof isRich);
---
+++ chapter-3/2.js
@@ -0,0 +1,34 @@
+//3.43
+let age=24;
+console.log(age);
+age=25;
+console.log(age);
+
+//3.44
+let pocketMoney=500;
+pocketMoney=150;
+console.log(pocketMoney);
+
+//3.45
+let studyTime=4;
+studyTime=6;
+console.log(studyTime);
+
+//3.46
+let friendsCount=3;
+console.log(friendsCount-2);
+
+//3.47
+let phonePrice=25000;
+phonePrice=12000;
+console.log(phonePrice);
+
+//3.48
+let chairsCount=40;
+chairsCount=40-2;
+console.log(chairsCount);
+
+//3.49
+let playTime=3;
+playTime=3+2;
+console.log(playTime);
