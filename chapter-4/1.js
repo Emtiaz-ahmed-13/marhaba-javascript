28dac90783325ab7ecf1c9318fb093c3d378af8a
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-11 15:13:18 +0600

    complete chapter-4 half

---
+++ chapter-4/1.js
@@ -0,0 +1,41 @@
+//4.71
+let myselft=7;
+let mybrother=5;
+console.log(myselft>mybrother);
+
+//4.72
+let MyClass=45;
+let otherClass=50;
+console.log(MyClass<otherClass);
+
+//4.73
+let myNumber=75;
+let myFriend=75;
+console.log(myNumber==myFriend);
+
+//4.74
+let myTarget=100;
+let myScore=95;
+if (myTarget>myScore){
+    console.log("I will try harder");
+}
+else{
+    console.log("I will try my best");
+}
+
+//4.75
+let diff=10;
+console.log(diff != 7);
+
+//4.76
+console.log(20<=15);
+
+//4.77
+let myScore1=45;
+let myFriendScore=95;
+if (myScore1<myFriendScore){
+    console.log("my Score is less than my friend");
+}
+else{
+    console.log("I will try my best");
+}
