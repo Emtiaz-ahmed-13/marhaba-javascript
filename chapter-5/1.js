72f0c636137eb51a93a097e2cba7ef13a39bfb28
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-13 00:06:42 +0600

    chapter-5 half complete

---
+++ chapter-5/1.js
@@ -0,0 +1,47 @@
+// 5.93
+let isCommingHome = 6;
+if (isCommingHome) {  
+    console.log('ma will give me nasta');
+} else {
+    console.log('ma will give me gharur bari');
+}
+
+// 5.94
+let isLoggedIn = true;
+if (isLoggedIn) {  
+    console.log('welcome!');
+} else {
+    console.log('Get Lost!');
+}
+
+// 5.95
+let run = '5km';
+if (run) {  
+    console.log('ami chocolate khete parbo');
+} else {
+    console.log('ami chocolate khete parbo na');
+}
+
+// 5.96 
+let isPassed = 80;
+if (isPassed >= 80) {  
+    console.log('amke bike kine dibe');
+} else {
+    console.log('amke bike kine dibe na');
+}
+
+// 5.97
+let movieShow = '9pm';
+if (movieShow) {  
+    console.log('ami movie dekte jabo');
+} else {
+    console.log('ami movie dekte jabo na');
+}
+
+// 5.98
+let temperature = 30;
+if (temperature) {  
+    console.log('conditionar chalabo');
+} else {
+    console.log('ami kombol muri diya ghumabo');
+}
---
+++ chapter-5/2.js
@@ -0,0 +1,68 @@
+// 5.99
+let myAge = 18;
+let myHeight = '60inh';
+if (myAge > 18 && myHeight === '60inh') {  
+    console.log('ami gari chalaite parbo');
+} else {
+    console.log('ami gari chalaite parbo na');
+}
+// 5.100
+let studentGpa = 5;
+let FamilyMonthlyIncome = 10000;
+if (studentGpa === 5 && FamilyMonthlyIncome < 10000) {  
+    console.log('you will get a scholarship');
+} else {
+    console.log('you will not get a scholarship');
+}
+// 5.101
+let MyAge = 18;
+let experience = 2;
+if (MyAge < 30 || experience > 2) {  
+    console.log('ami chakrir porikkha dite parbo');
+} else {
+    console.log('ami chakrir porikkha dite parbo na');
+}
+
+// 5.102
+let Eggnumber = 12;
+let MugirDim = true;
+if (Eggnumber > 12 || MugirDim) {  
+    console.log('ami mugir kurma ranna korbo');
+} else {
+    console.log('ami mugir kurma ranna korbo na');
+}
+
+// 5.103
+let temperature = 100;
+if (temperature > 100) {  
+    console.log('amr kasi hoiase doctore ar kache jabo');
+} else {
+    console.log('ami khata muri diya shuya thakbo');
+}
+// 5.104
+let attendance = 80;
+if (attendance > 80) {  
+    console.log('homework jome ase');
+} else {
+    console.log('fail');
+}
+
+// 5.105
+let electrocity = true;
+let mobileCharge = true;
+
+if (electrocity && mobileCharge) {  
+    console.log('video game khelbo');
+} else {
+    console.log('video game khelbo na');
+}
+
+// 5.106
+let TshirtPrice = 1000;
+let Coupon = true;
+
+if (TshirtPrice < 1000 && Coupon) {  
+    console.log('I will buy the T-shirt');
+} else {
+    console.log('I will not buy the T-shirt');
+}
