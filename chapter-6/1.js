3bac0417f226e23581fb8a9409a20d1319242955
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-15 01:28:00 +0600

    finish half chapter 6

---
+++ chapter-6/1.js
@@ -0,0 +1,28 @@
+
+//6.126
+let arr=[71,72,73,74,75,76,77,78,79];
+console.log(arr)
+
+//6.127
+let myFrige=['apple','banana','orange','grapes',];
+console.log(myFrige)
+
+//6.128
+let market=['alu','pias','begun','mula','gajor','tomato','lau','shoha']
+console.log(market)
+
+//6.129
+let myFavMovie=['babubali','sultan','3idiots','pk','dangal','babubali2']
+console.log(myFavMovie)
+
+//6.130
+let oddNumber=[11,13,15,17,19,21,23,25,27,29]
+console.log(oddNumber)
+
+//6.131
+let colour = ['red', 'green', 'blue'];
+console.log(colour.length);
+
+//6.132
+let capitalCountry=['dhaka','delhi','kathmandu','colombo','islamabad','kabul']
+console.log(capitalCountry.length)
---
+++ chapter-6/2.js
@@ -0,0 +1,31 @@
+
+//6.133
+let myFriendName=['mamum','sakib','Rauf','lotifur','sadman']
+console.log(myFriendName[3])
+
+//6.134
+let myFavoriteBooks=['MarhabaJavascript','Opekkha','Brave new World','To kill a mockingbird','The Great Gatsby','The Catcher in the Rye']
+console.log(myFavoriteBooks[5])
+
+
+//6.135
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+numbers[7]=30;
+console.log(numbers)
+
+//6.136
+let mobileGames=['Free-Fire','PUBG','candy-crash','Temple Run']
+mobileGames[2]='Subway Surfers'
+console.log(mobileGames);
+
+//6.137
+let MyFamilyName=['Aslam','Jubayda','mahi','efty']
+console.log(MyFamilyName[3])
+
+//6.138
+let sevenCountry=['Bangladesh','India','Pakistan','Srilanka','England','Bhutan','Maldives']
+console.log(sevenCountry[4])
+
+//6.139
+let TableHave=['Chair','Bed','Sofa','Dining Table','Dressing Table','Study Table','Center Table','Side Table']
+console.log(TableHave[7])
