cdbaed7718e7d7b48311c2405a2deff9195e6fd9
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-15 02:11:20 +0600

    finish chapter-6

---
+++ chapter-6/3.js
@@ -0,0 +1,26 @@
+
+//6.140
+let numbers=[10,20,30,40,50];
+numbers.push(60);
+console.log(numbers);
+
+//6.141
+let friendNames=['sojib','sagor','sajib','shohel','sakib',];
+friendNames.push('sumon');
+console.log(friendNames);
+
+//6.142
+let mobileGame=['pubg','freefire','cod','clash of clans','clash royale'];
+mobileGame.pop();
+console.log(mobileGame);
+
+
+//6.143
+let elements=[24,36,48,60]
+elements.unshift(12);
+console.log(elements);
+
+//6.144
+let books=['harry potter','lord of the rings','the hobbit','the silmarillion','the fellowship of the ring'];
+books.shift();
+console.log(books);
---
+++ chapter-6/4.js
@@ -0,0 +1,28 @@
+// 6.145
+let mangoExit = ['mango', 'apple', 'banana', 'grapes', 'orange'];
+mangoExit.includes('mango') ? console.log('am ase') : console.log('am nei gashe uth');
+
+// 6.146
+let birds = ['babul', 'alif', 'choton'];
+console.log(birds.indexOf('babul'));
+
+// 6.147
+let friendsNames = ['rimon', 'rifat', 'rajib'];
+console.log(friendsNames.indexOf('rifat')); 
+
+// 6.148
+let city = ['dhaka', 'chittagong', 'sylhet'];
+city.push('rajshahi');
+city.includes('rajshahi') ? console.log('ase') : console.log('nei');
+
+// 6.149 
+let rain = ['megh', 'bristy', 'borsha'];
+if (rain.includes('bristy')) {
+    console.log('I need umbrella'); 
+} else {
+    console.log('No rain no pain'); 
+}
+
+// 6.150
+let favPlayGame = ['cricet', 'football', 'badminton', 'volleyball'];
+favPlayGame.includes('badminton') ? console.log('ase') : console.log('nei');
