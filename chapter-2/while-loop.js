0da4c4047d2c1b7b0bc153d936425349a05e782c
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-17 00:02:37 +0600

    finish chapter-2

--- chapter-2/while-loop.js
+++
@@ -1,7 +0,0 @@
-// Program to print message 10 times using while loop
-let count = 1;  // Starting from 1
-
-while (count <= 10) {
-    console.log(count + ". Ami protidin minimum 3h kore");
-    count++;  // Increment counter
-} 
