67d5bc4af856fe00f1aed6d9d45199d2dc274736
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-16 23:22:16 +0600

    finish half chapter 7

---
+++ chapter-7/1.js
@@ -0,0 +1,20 @@
+//7.151
+let favBook=['physcis','chemistry','math','biology','english'];
+for(const num of favBook){
+    console.log(num);
+}
+//7.152
+let favFood=['dim','alur vorta','murgi','kacchi'];
+for(const num of favFood){
+    console.log(num);
+}
+//7.153
+let favCrickeCountry=['joy','bistrow','stokes','root','buttler','morgan','archer','wood'];
+for(const num of favCrickeCountry){
+    console.log(num);
+}
+//7.154
+let examDate=['19','24']
+for(const num of examDate){
+    console.log(num);
+}
---
+++ chapter-7/2.js
@@ -0,0 +1,34 @@
+//7.155
+let count = 1; 
+while (count <= 10) {
+    console.log("Ami protidin minimum 3h kore");
+    count++; 
+} 
+//7.156
+let counter=1;
+while(counter <=20){
+    console.log(counter)
+    counter++;
+}
+//7.157
+let printnumber=50;
+while(printnumber<=100){
+    console.log(printnumber)
+    printnumber++;
+}
+//7.158
+let number = 1;
+while (number <= 10) {
+    if (number % 3 == 0) {
+        console.log(number);
+    }
+    number++; 
+}
+//7.159
+let num = 111;
+while (num >= 110) {
+    console.log(num / 2); 
+    num--; 
+}
+//7.160
+
