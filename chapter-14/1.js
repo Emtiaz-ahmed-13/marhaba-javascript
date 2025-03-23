689302f78e365a4fec562df4a61f83a4dda37db5
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-03-24 00:25:33 +0600

    finish half chapter-14

---
+++ chapter-14/1.js
@@ -0,0 +1,27 @@
+//14.333
+const city="Chattogram";
+console.log(city.lastIndexOf("g"));
+
+//14.334s
+const division="sylhet";
+console.log(division.includes("y"));
+
+//14.335
+const name="Rifat";
+console.log(name[name.length-1]);
+
+//14.336
+const language="JavaScript";
+console.log(language.indexOf("rip"));
+
+//14.337
+const text = "Immutable";
+console.log(text.length); 
+
+//14.338
+let text1 = "Immutable";
+text[0] = "X"; 
+console.log(text1); 
+
+let newText = "Immutable".replace("I", "X");
+console.log(newText);
